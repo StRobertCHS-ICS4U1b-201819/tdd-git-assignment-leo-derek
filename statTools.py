c3b892ec3c089b632c9e62e1a12703fa67eee76e
robuntu <robuntu@stro.ycdsb.ca>
2018-11-06 10:25:43 -0500

    create test for when number of terms in the list is even

--- statTools.py
+++ statTools.py
@@ -1,8 +1,22 @@
 
 def median(num):
     value = 0
+
     numlist = list(num)
 
+    number_of_terms = len(numlist)
 
 
-    return value
+    # bubble sort the list
+    has_swapped = True
+    while has_swapped:
+        has_swapped = False
+
+        for i in range(n - 1):
+            if numlist[i + 1] < numlist[i]:
+                numlist[i + 1], numlist[i] = numlist[i], numlist[i + 1]
+                has_swapped = True
+          i += 1
+
+        if number_of_terms % 2 == 0:
+            return numlist[number_of_terms/2]
