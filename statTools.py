66fbafed49a6aadd6fbdf3a753aef48aa89a3ecd
robuntu <robuntu@stro.ycdsb.ca>
2018-11-06 10:57:38 -0500

    replace bubble sort with sorted function

--- statTools.py
+++ statTools.py
@@ -7,18 +7,10 @@ def median(num):
     print(number_of_terms)
 
     # bubble sort the list
-    has_swapped = True
-    while has_swapped:
-        has_swapped = False
-
-        for i in range(number_of_terms - 1):
-            if numlist[i + 1] < numlist[i]:
-                numlist[i + 1], numlist[i] = numlist[i], numlist[i + 1]
-                has_swapped = True
-            i += 1
+    numlist = sorted(numlist)
 
     if number_of_terms % 2 == 1:
-        value = numlist[int(number_ofgit_terms/2)]
+        value = numlist[int(number_of_terms/2)]
     elif number_of_terms == 0:
         value = 0
     else:
