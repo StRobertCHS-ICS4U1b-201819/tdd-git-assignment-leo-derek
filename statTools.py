28c52ce0ec2a7a2719da55f52ca4c37a0aa1c77f
robuntu <robuntu@stro.ycdsb.ca>
2018-11-06 10:50:32 -0500

    fix some problems with the math

--- statTools.py
+++ statTools.py
@@ -3,20 +3,24 @@ def median(num):
     value = 0
 
     numlist = list(num)
-
-    number_of_terms = len(numlist)
-
+    number_of_terms = int(len(numlist))
+    print(number_of_terms)
 
     # bubble sort the list
     has_swapped = True
     while has_swapped:
         has_swapped = False
 
-        for i in range(n - 1):
+        for i in range(number_of_terms - 1):
             if numlist[i + 1] < numlist[i]:
                 numlist[i + 1], numlist[i] = numlist[i], numlist[i + 1]
                 has_swapped = True
-          i += 1
+            i += 1
 
-        if number_of_terms % 2 == 0:
-            return numlist[number_of_terms/2]
+    if number_of_terms % 2 == 1:
+        value = numlist[int(number_ofgit_terms/2)]
+    elif number_of_terms == 0:
+        value = 0
+    else:
+        value = (numlist[int(number_of_terms/2) - 1] + numlist[int(number_of_terms/2)])/2
+    return value
--- test_statTools.py
+++ test_statTools.py
@@ -1,13 +1,13 @@
 import pytest
 from statTools import*
 def test_median_basic1():
-   assert(median(3, 4, 5, 6, 7) == 5)
+   assert(median([3, 4, 5, 6, 7]) == 5)
 
 def test_median_basic2():
-   assert(median(3,4,2,1,5,3,7,2) == 3)
+   assert(median([3,4,2,1,5,3,7,2]) == 3)
 
 def test_median_basic3():
-   assert(median(2,3) == 2.5)
+   assert(median([2,3]) == 2.5)
 
 def test_median_basic4():
-   assert(median(0) == 0)
+   assert(median([0]) == 0)
