23ae345661c6efc66e71dc8cb1274c98445e4ffa
robuntu <robuntu@stro.ycdsb.ca>
2018-11-06 10:30:59 -0500

    finish mean code without refractors

--- test_statTools.py
+++ test_statTools.py
@@ -1,1 +1,11 @@
+import pytest
+from statTools import *
 
+def test_mean():
+    mean_list = [50, 100, 75, 25]
+    x = sum(mean_list)
+    y = 0
+    for i in mean_list:
+        y = y + 1
+    mean = x/y
+    print(mean)
