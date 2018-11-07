8baad99f427f470009226db13f6fb4b8b97ffe78
robuntu <robuntu@stro.ycdsb.ca>
2018-11-07 10:48:59 -0500

    create test case, complete mean and mode function

--- statTools.py
+++ statTools.py
@@ -1,2 +1,17 @@
-def mean():
-    pass
+def mean(list):
+    x = sum(list)
+    y = 0
+    for i in list:
+        y = y + 1
+    if y == 0:
+        mean = 0
+    else:
+        mean = x / y
+    return(mean)
+
+def mode(list):
+    if len(list) == 0:
+        return 0
+    else:
+        mode = max(set(list), key=list.count)
+        return mode
--- test_statTools.py
+++ test_statTools.py
@@ -1,11 +1,25 @@
 import pytest
 from statTools import *
+def test_mean_basic1():
+    assert(mean([10]) == 10)
 
-def test_mean():
-    mean_list = [50, 100, 75, 25]
-    x = sum(mean_list)
-    y = 0
-    for i in mean_list:
-        y = y + 1
-    mean = x/y
-    print(mean)
+def test_mean_basic2():
+    assert(mean([10, 20, 30]) == 20)
+
+def test_mean_basic3():
+    assert(mean([]) == 0)
+
+def test_mean_basic4():
+    assert(mean([0.1, 0.2, 0.3]) == 0.20000000000000004)
+
+def test_mode_basic1():
+    assert(mode([0]) == 0)
+
+def test_mode_basic2():
+    assert(mode([0, 1, 1, 2, 3]) == 1)
+
+def test_mode_basic3():
+    assert(mode([0.1, 0.1, 0.3]) == 0.1)
+
+def test_mode_basic4():
+    assert(mode([]) == 0)
