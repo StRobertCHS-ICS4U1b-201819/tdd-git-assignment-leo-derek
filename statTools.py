49dc8bf80c04db4eed326860740cd1765b753690
robuntu <robuntu@stro.ycdsb.ca>
2018-11-15 10:41:47 -0500

    Make comments for range function

--- statTools.py
+++ statTools.py
@@ -16,16 +16,13 @@ def median(num: list):
         # Sorts the list from least to greatest
         num.sort()
 
-        # If the number of items in the list is an odd number, value is the number in the middle
+        # If the number of items in the list is an odd number, returns the number in the middle
         if len(num) % 2 == 1:
-            value = num[int(len(num)/2)]
+            return num[int(len(num)/2)]
 
-        # If the number of items in the list is an even number, value is the mean of the two middle numbers
+        # If the number of items in the list is an even number, returns the mean of the two middle numbers
         else:
-            value = (num[int(len(num)/2) - 1] + num[int(len(num)/2)])/2
-
-        # Returns the value
-        return value
+            return (num[int(len(num)/2) - 1] + num[int(len(num)/2)])/2
 
     # Raises an error if a string is inputted or contains a string in the list
     except TypeError:
@@ -43,20 +40,28 @@ def range(num: list):
       """
 
     try:
+
+        # Raises an error if the list contains less than 2 objects
         if len(num) < 2:
             raise ValueError('Illegal empty list or list too short')
 
+        # Sorts the list from least to greatest
         num.sort()
 
+        # If the number of objects in the list is greater than 1, returns the difference of the last number in the list
+        # and the first number in the list
         if len(num) >= 2:
-            return num[len(num) -1] - num[0]
+            return num[len(num) - 1] - num[0]
 
+        # If there is only one object in the list, returns that value
         elif len(num) == 1:
             return num[0]
 
+    # Raises an error if a string is inputted or contains a string in the list
     except TypeError:
         raise TypeError("A list was not provided or no numbers were found in list.")
 
+    # Raises an error if a number not in a list is inputted
     except AttributeError:
         raise AttributeError("A list was not provided.")
 
