525b0094e240e2bb2ff48ca6cf0f55689236566c
Your Name <you@example.com>
2018-11-18 22:53:25 -0500

    merge member1-dev to master

--- statTools.py
+++ statTools.py
@@ -1,1 +1,138 @@
+import math
 
+# ----------------------------------------------------------------------------------------------------- #
+# Filename: statTools.py                                                                                #
+# Purpose: functions that find statistics: mean, mode, variance, standard deviation                     #
+# Author: L. Xiao                                                                                       #
+# Created: 31/10/2018                                                                                   #
+# ----------------------------------------------------------------------------------------------------- #
+
+
+def mean(data_set):
+    """Given a list of numbers, return the mean of that list
+    :param data_set: list of numbers
+    :return: mean of a list of numbers
+    """
+
+    try:
+
+        # Check to see if list has more than one number
+        if len(data_set) < 1:
+            return None
+        else:
+
+            # Adds all the numbers in the list and then divides that by the amount of numbers in the list and
+            # rounds to two decimals to avoid python rounding errors
+            return round(sum(data_set) / len(data_set), 2)
+
+        # Raises type error if a list of integers was not found
+    except TypeError:
+        raise TypeError("Error: please input a list of integers")
+
+
+def mode(data_set):
+    """Given a list of numbers, return the mode of that list
+    :param data_set: list of numbers
+    :return: the mode of that list of numbers
+    """
+
+    try:
+        # Check to see if the list has any numbers
+
+        int_list = [int(i) for i in data_set]
+
+        if len(int_list) < 1:
+            return None
+
+        # Counts the number of times each value appears in the list and returns the most common number
+        else:
+            most_common_number = max(set(int_list), key=int_list.count)
+
+            return most_common_number
+
+    # Raises value error if a list of strings were found
+    except ValueError:
+        raise ValueError("Error: please input a list of integers")
+
+
+def variance(data_set):
+    """Given a list of numbers, return the variance of that list
+    :param data_set: list of numbers
+    :return: the variance of that list of numbers
+    """
+
+    # Check to see if the list has 2 or more numbers since you require more than 1 number to find variance
+    if len(data_set) <= 1:
+        return None
+
+    else:
+        try:
+
+            # Initialize the variables for easy access later
+            n = len(data_set)
+            mean_of_data = sum(data_set) / len(data_set)
+
+            # Initialize lists to use later
+            subtract_mean_from_data = []
+            square_of_each_point = []
+
+            # Subtracts all numbers in the list by the mean and stores that in a new list
+            i = 0
+            while i <= n - 1:
+                x = data_set[i] - mean_of_data
+                subtract_mean_from_data.append(x)
+                i = i + 1
+
+            # Squares all the numbers in the new list and stores those numbers in another new list
+            j = 0
+            while j <= n - 1:
+                b = subtract_mean_from_data[j] * subtract_mean_from_data[j]
+                square_of_each_point.append(b)
+                j = j + 1
+
+            # Divide the sum of the second new list by the length of the initial list to calculate variance
+            variance_of_data = (sum(square_of_each_point) / (n - 1))
+
+            return variance_of_data
+
+        # Raises type error if a list of integers was not found
+        except TypeError:
+            raise TypeError("Error: please input a list of integers")
+
+
+def standard_deviation(data_set):
+    """Given a list of numbers, return the standard deviation of that list
+    :param data_set: list of numbers
+    :return: the standard deviation of that list of numbers
+    """
+
+    # Calculates the variance
+    if len(data_set) <= 1:
+        return None
+    else:
+        try:
+            n = len(data_set)
+            mean_of_data = sum(data_set) / len(data_set)
+            subtract_mean_from_data = []
+            square_of_each_point = []
+
+            i = 0
+            while i <= n - 1:
+                x = data_set[i] - mean_of_data
+                subtract_mean_from_data.append(x)
+                i = i + 1
+
+            j = 0
+            while j <= n - 1:
+                b = subtract_mean_from_data[j] * subtract_mean_from_data[j]
+                square_of_each_point.append(b)
+                j = j + 1
+
+            variance_of_data = (sum(square_of_each_point) / (n - 1))
+
+            # Square roots the variance to calculate standard deviation
+            return float("%0.2f" % (math.sqrt(variance_of_data)))
+
+        # Raises type error if a list of integers was not found
+        except TypeError:
+            raise TypeError("Error: please input a list of integers")
--- test_statTools.py
+++ test_statTools.py
@@ -1,1 +1,115 @@
 
+import pytest
+from statTools import *
+
+
+def test_mean_basic1():
+    assert(mean([10]) == 10)
+
+
+def test_mean_basic2():
+    assert(mean([10, 20, 30]) == 20)
+
+
+def test_mean_basic3():
+    assert(mean([0.1, 0.2, 0.3]) == 0.2)
+
+
+def test_mean_basic4():
+    assert(mean([0]) == 0)
+
+
+def test_mean_basic5():
+    assert(mean([]) is None)
+
+
+def test_mean_type_error():
+    with pytest.raises(TypeError) as errmsg:
+        mean(["a"])
+    assert ("Error: please input a list of integers" == str(errmsg.value))
+
+
+def test_mode_basic1():
+    assert(mode([1, 2, 3, 3, 3]) == 3)
+
+
+def test_mode_basic2():
+    assert(mode([1, 2, 2, 2, 2, 3, 3, 3, 3, 3]) == 3)
+
+
+def test_mode_basic3():
+    assert(mode([1, 2, 2, 3, 3, 3, 3, 3]) == 3)
+
+
+def test_mode_basic4():
+    assert(mode([1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3]) == 2)
+
+
+def test_mode_basic5():
+    assert(mode([]) is None)
+
+
+def test_mode_value_error():
+    with pytest.raises(ValueError) as errmsg:
+        mode(["a", "b", "c"])
+    assert("Error: please input a list of integers" == str(errmsg.value))
+
+
+def test_variance_basic1():
+    assert(variance([17, 15, 23, 7, 9, 13]) == 33.2)
+
+
+def test_variance_basic2():
+    assert(variance([1, 2, 3, 4, 5, 6]) == 3.5)
+
+
+def test_variance_basic3():
+    assert(variance([10, 20, 30]) == 100)
+
+
+def test_variance_basic4():
+    assert(variance([1, 10, 10, 20, 46, 240]) == 8499.1)
+
+
+def test_variance_basic5():
+    assert(variance([1, 2]) == 0.5)
+
+
+def test_variance_basic6():
+    assert(variance([]) is None)
+
+
+def test_variance_type_error():
+    with pytest.raises(TypeError) as errmsg:
+        variance(["a", "b", "c"])
+    assert("Error: please input a list of integers" == str(errmsg.value))
+
+
+def test_standard_deviation_basic1():
+    assert(standard_deviation([10, 20, 30]) == 10)
+
+
+def test_standard_deviation_basic2():
+    assert(standard_deviation([1, 10, 10, 20, 46, 240]) == 92.19)
+
+
+def test_standard_deviation_basic3():
+    assert(standard_deviation([1, 2, 3, 4, 5, 6]) == 1.87)
+
+
+def test_standard_deviation_basic4():
+    assert(standard_deviation([0, 1, 1, 2, 2, 3]) == 1.05)
+
+
+def test_standard_deviation_basic5():
+    assert(standard_deviation([1, 2]) == 0.71)
+
+
+def test_standard_deviation_basic6():
+    assert(standard_deviation([]) is None)
+
+
+def test_standard_deviation_type_error():
+    with pytest.raises(TypeError) as errmsg:
+        standard_deviation(["a", "b", "c"])
+    assert("Error: please input a list of integers" == str(errmsg.value))
